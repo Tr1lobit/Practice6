4bd37e02cd8a2eb39707aa4b039fdf764a671b11
Tr1lobit <Tr1lobit@TR1LOBIT>
2024-02-28 20:15:54 +0300

    Добавьте файлы проекта.

---
+++ WpfCafe/View/Windows/AddShiftWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfCafe.View.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AddShiftWindow.xaml
+    /// </summary>
+    public partial class AddShiftWindow : Window
+    {
+        public AddShiftWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddShiftBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WpfCafe/View/Windows/ChefWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfCafe.View.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для ChefWindow.xaml
+    /// </summary>
+    public partial class ChefWindow : Window
+    {
+        public ChefWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
